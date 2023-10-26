49bf6d5a7df2fc2d899bab3ea74b987426b6e1bd
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-10-26 19:12:04 +0200

    add cartCount

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -20,7 +20,7 @@ const CartDropdown = () => {
             <span className='empty-message'>Your cart is empty</span>
           )}
         </div>
-        <Button>GO TO CHECKOUT</Button>
+        <Button>CHECKOUT</Button>
       </div>
     )
 };
