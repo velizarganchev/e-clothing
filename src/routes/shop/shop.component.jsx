685ecd9601b979018bb30bc4d82b964d212d4c04
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-10-26 14:18:13 +0200

    create productContext

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+
+import { ProductsContext } from "../../contexts/products.context";
 
 const Shop = () => {
+    const { products } = useContext(ProductsContext);
+
     return (
-        <div><h1>Shop</h1></div>
+        <div>
+            <h1>Shop</h1>
+            {
+                products.map(({ id, name }) => (
+                    <div key={id}>
+                        <h3>{name}</h3>
+                    </div>
+                ))}
+        </div>
     )
 }
 
