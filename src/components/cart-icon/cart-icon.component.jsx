9684892b0fb7253b38d8b625c115f768539883c6
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-10-26 18:31:25 +0200

    add cartDropdown,cartIcon, cartItem,productCard

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
-import { CatrContext } from "../../contexts/cart.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CatrContext);
+    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
 
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
     return (
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,7 +2,7 @@ import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import { UserContext } from "../../contexts/user.context";
-import { CatrContext } from "../../contexts/cart.context";
+import { CartContext } from "../../contexts/cart.context";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
@@ -13,7 +13,7 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 const Navigation = () => {
 
     const { currentUser } = useContext(UserContext);
-    const { isCartOpen } = useContext(CatrContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
