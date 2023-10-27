90523a57716527738844476ca879c033c243868c
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-10-27 17:23:44 +0200

    create addCollection und getCategories in fireBase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,21 @@
-import { createContext, useState } from "react";
-
-import SHOP_DATA from "../shop-data.js";
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesUndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
+
     const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesUndDocuments();
+        };
+
+        getCategoriesMap();
+    }, [])
     const value = { products };
 
     return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -12,7 +12,11 @@ import {
     getFirestore,
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs,
 
 } from "firebase/firestore"
 
@@ -38,6 +42,37 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const db = getFirestore();
+
+export const addCollectinAndDocuments = async (collectionKey, objectToAdd) => {
+
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectToAdd.forEach((object) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    })
+    await batch.commit();
+    console.log("done");
+};
+
+export const getCategoriesUndDocuments = async () => {
+
+    const collectionRef = collection(db, "categories");
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+
+        return acc;
+    }, {})
+
+    return categoryMap;
+};
+
 
 export const createUserDocumetnFromAuth = async (
     userAuth,
